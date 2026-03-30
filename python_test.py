d4d5656560907b2fe2064509e167a14471fe06cf
kaushal-nickelfox <kaushal.gupta@nickelfox.com>
2026-03-30 17:23:37 +0530

    chore: add test script to push

---
+++ python_test.py
@@ -0,0 +1,11 @@
+#hi this is the file where i can have some free 
+#code to test some of the functionalities, this file will not be part of the final codebase, it is just for testing purposes
+
+#i can write a function to add two numbers and test it here
+#and then i will try to push it to the main brach to test whether github secret scanning is working or not
+
+def add_numbers(a, b):
+    return a + b
+
+
+print(add_numbers(2, 3))
